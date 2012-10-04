9833bec66e935b693f044d0872bd6f42dfbfc63d
William Basmayor <wbasmayor@gmail.com>
2012-10-04 10:28:03 +0800

    adding find_by_masked_id  and removing extension of class method find

--- lib/masked_id.rb
+++ lib/masked_id.rb
@@ -11,12 +11,15 @@ module MaskedId
     MaskedId::Baffler.baffle_id(self.id, self.object_id)
   end
 
-  module ModelExtensions
-
-    def find(id)
-      super(MaskedId::Baffler.debaffle_id(id, self.object_id))
-    end
-
+  def find_by_masked_id(id)
+    self.find(MaskedId::Baffler.debaffle_id(id, self.object_id))
   end
+  #module ModelExtensions
+
+  #  def find(id)
+  #    super(MaskedId::Baffler.debaffle_id(id, self.object_id))
+  #  end
+
+  #end
 
 end
